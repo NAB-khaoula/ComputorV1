4c56576ae53e707cc4941de29dfec7fac5ca0410
khaoula NABOUSSI <k12.naboussi@gmail.com>
2022-12-26 15:34:49 +0100

    Parsing v-1: parsing the equation without error handling

--- starter.js
+++ starter.js
@@ -1,16 +1,20 @@
 const firstPart = process.argv[2].split("=")[0].replace(/[-]/g, '+-').split(/[+]/).filter((e) => e.trim().length > 0 );
 const secondPart = process.argv[2].split('=')[1].replace(/[-]/g, '+-').split(/[+]/).filter((e) => e.trim().length > 0);
-const equationElement = firstPart.concat(secondPart);
-console.log('first part', firstPart);
-console.log('second part', secondPart);
-console.log('equation Elements', equationElement);
-let arr = [];
-const test = equationElement.map((e) => {
-    arr.push({
-        coeff: e.split(/[*]/)[0],
-        puissance: e.split(/[*]/)[1].split('^')[1]
+let firstPartArr = [];
+let secondPartArr = [];
+firstPart.map((e) => {
+    firstPartArr.push({
+        coeff: Number(e.replace(' ', '').split(/[*]/)[0]),
+        puissance: Number(e.replace(' ', '').split(/[*]/)[1].split('^')[1])
     })
 })
-console.log(arr)
-// const arrayElement = {Coeff: }
+secondPart.map((e) => {
+    secondPartArr.push({
+        coeff: Number(e.replace(' ', '').split(/[*]/)[0]) * (-1),
+        puissance: Number(e.replace(' ', '').split(/[*]/)[1].split('^')[1])
+    })
+})
+const equationElement = firstPartArr.concat(secondPartArr);
+
+console.log('equation Elements', equationElement);
 
